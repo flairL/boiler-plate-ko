846d2e305dd81937aa5769355ceaa00b57f2d742
Pearl Lee <pearllee@Pearls-MacBook-Air.local>
2021-10-05 18:31:54 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://flair:<mflair2150!>@youtubeclone.zpjrf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }).then(() => console.log('MongoDB Connected..'))
+    .catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
