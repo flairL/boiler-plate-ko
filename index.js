640b126aacd790966d49c3e016edc117c844b28c
Pearl Lee <pearllee@Pearls-MacBook-Air.local>
2021-10-06 15:50:15 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://flair:<mflair2150!>@youtubeclone.zpjrf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
+
+
+const mongoose = require('mongoose');
+
+mongoose.connect(config.mongoURI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    }).then(() => console.log('MongoDB Connected..'))
+        //useCreateIndex: true,
+        //useFindAndModify: false
+    }).then(() => console.log('MongoDB Connected success!!'))
     .catch(err => console.log(err))
 
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! 아름다운 당신의 미래를 위하여!'))
+
+app.post('/register', (req, res) => {
+    //req = request, res = response
+    //회원가입할 때  필요한 정보를 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+
+
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
